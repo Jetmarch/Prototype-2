1c3506861a92c9b7da4c9b9424d17bd693243964
Jet <j3tmarch@gmail.com>
2021-08-08 17:08:34 +0300

    Lesson 2.1

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed = 10.0f;
+    public float distanceToInvisibleWall = 10;
+    private float horizontalInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < -distanceToInvisibleWall)
+        {
+            transform.position = new Vector3(-distanceToInvisibleWall, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > distanceToInvisibleWall)
+        {
+            transform.position = new Vector3(distanceToInvisibleWall, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * playerSpeed * horizontalInput);
+    }
+}
