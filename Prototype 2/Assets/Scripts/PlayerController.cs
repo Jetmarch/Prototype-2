ab15c417719f9b21c50aacc5708dbe9e1d3ece68
Jet <j3tmarch@gmail.com>
2021-08-09 19:37:08 +0300

    Lesson 2.2

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float playerSpeed = 10.0f;
     public float distanceToInvisibleWall = 10;
+    public GameObject projectilePrefab;
+
     private float horizontalInput;
     
     // Start is called before the first frame update
@@ -27,5 +29,10 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * playerSpeed * horizontalInput);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
