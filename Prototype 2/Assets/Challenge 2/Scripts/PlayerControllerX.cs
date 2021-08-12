df948dcf04f545272a626cfc4445800a112f131e
Jet <j3tmarch@gmail.com>
2021-08-12 20:17:21 +0300

    Challenge 2

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float dogCooldown = 1.0f;
+    private bool isDogReady;
+
+    private void Start()
+    {
+        isDogReady = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && isDogReady)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            isDogReady = false;
+            Invoke("DogReady", dogCooldown);
+        }
+    }
+
+    void DogReady()
+    {
+        isDogReady = true;
+        Debug.Log("Dog is ready!");
+    }
+}
